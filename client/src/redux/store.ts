fe7eb1ebd732ec80d39d03b9df125a1addfb6c2f
saurav <sauravdeshmukh5@gmail.com>
2024-01-28 12:00:19 +0530

    add redux persist

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,15 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice.ts'
-export const store = configureStore({
-  reducer: {
-    user:userReducer
-  },
-  middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
-    serializableCheck: false, // Immutable.js isn't serializable
-  })
-})
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice.ts";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["router"],
+  timeout: null, // added this to fix root timeout warning
+} as any;
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Immutable.js isn't serializable
+    }),
+});
+
+export const persistor = persistStore(store);
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
