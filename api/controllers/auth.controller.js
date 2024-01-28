f9a356d2eed4638d3614f5cad6f3e98459bede6e
saurav <sauravdeshmukh5@gmail.com>
2024-01-28 10:38:43 +0530

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,34 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 
-export const singup = async (req, res,next) => {
+export const singup = async (req, res, next) => {
     const { username, email, password } = req.body;
-    const hashedPassword=bcryptjs.hashSync(password,10);
-    console.log(hashedPassword)
-    const newUser = new User({ username, email, password:hashedPassword });
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
     try {
         await newUser.save();
         res.status(201).json('User created successfully');
     } catch (error) {
         next(error);
     }
-   
+
+}
+
+export const singin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not found!'));
+        const isValidPassord = bcryptjs.compareSync(password, validUser.password);
+        if (!isValidPassord) return next(errorHandler(401, "Wrong Credentials!"));
+        //create token
+        const { password: pass, ...rest } = validUser._doc;
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+    } catch (error) {
+        next(error)
+    }
 }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { singup } from '../controllers/auth.controller.js';
+import { singin, singup } from '../controllers/auth.controller.js';
 
 const router =express.Router();
 
 router.post('/signup',singup)
+router.post('/signin',singin)
 
 export default router;
