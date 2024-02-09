a06923e13ca405134a6af5c8a96fbdc797d57777
saurav <sauravdeshmukh5@gmail.com>
2024-02-09 18:36:06 +0530

    create listing api route

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import cookieParser from "cookie-parser";
-
+import listingRouter from './routes/listing.route.js'
 
 dotenv.config();
 const app = express();
@@ -20,6 +20,7 @@ app.get('/test', (req, res) => {
 
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/listing', listingRouter)
 
 //error middleware
 app.use((err, req, res, next) => {
